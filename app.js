79a099f120b97fe5740d55908f43d58c9a5eb499
Brandon Tapia <btapia@ucol.mx>
2022-04-08 23:00:57 -0500

    Práctica 13

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from 'express'; //importamos la dependencia
+var app = new express(); //Declaramos una app de express
+
+var port = process.env.PORT || 3000; //seteamos el puerto para que escuche el servidor //indica el puerto por donde se ejecutará el server
+
+app.use('/assets', express.static('/public'));
+app.set('view engine', 'ejs');
+
+app.use('/', (req, res, next) => {
+    console.log('Request Url:' + req.url);
+    next();
+});
+
+//primera ruta(está al nivel de la raíz /), Hello World!
+app.get('/', (req, res) => {
+    res.render('index'); //imprime en html //Se cambia send por render porque un template se hará cargo de renderizar el contenido hacia index
+});
+
+//Tercera ruta
+app.get('/person/:id', (req, res) => {
+    res.render('person', { ID: req.params.id }); //Darle forma a la información en HTML. También recibiendo la información solicitada.//Solicitando el ID
+});
+
+app.get('/api', function (req, res){
+    res.json({firstname: 'John', lastname: 'Doe'}); //Envía información al server de JSON
+});
+
+app.get('/numbers/:id', (req, res) => {
+    res.render('numbers', { number: req.params.id }); //Darle forma a la información en HTML. También recibiendo la información solicitada.//Solicitando el número para evaluar si es par o no y así ejecutar cierta función.
+});
+
+app.listen(port); //levantar el server y ponerlo a escuchar. //Asignarle al server a que puerto debe escuchar
+
